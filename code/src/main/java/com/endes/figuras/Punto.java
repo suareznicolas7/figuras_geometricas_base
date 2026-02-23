3f9036d8d170a52a5ed7d6a3bd92686fda18daae
Ortega <ortega.romero.daniel@iescamas.es>
2026-02-23 14:01:45 +0100

    He acabado

--- code/src/main/java/com/endes/figuras/Punto.java
+++ code/src/main/java/com/endes/figuras/Punto.java
@@ -1,22 +1,50 @@
 package com.endes.figuras;
 
 public class Punto extends FiguraGeometrica {
+    
+    private double x;
+    private double y;
 
-	public Punto(String nombre, String color) {
-		super(nombre, color);
-		// TODO Auto-generated constructor stub
-	}
+    
+    public Punto(String nombre, String color, double x, double y) {
+        super(nombre, color); 
+        this.x = x;
+        this.y = y;
+    }
 
-	@Override
-	public double area() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    
+    public double getX() {
+        return x;
+    }
 
-	@Override
-	public double perimetro() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public void setX(double x) {
+        this.x = x;
+    }
 
+    
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    
+    public double distancia(Punto otro) {
+        double dx = this.x - otro.x;
+        double dy = this.y - otro.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    
+    @Override
+    public double area() {
+        return 0; 
+    }
+
+    @Override
+    public double perimetro() {
+        return 0; 
+    }
 }
