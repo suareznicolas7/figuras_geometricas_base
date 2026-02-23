bccf0307f7d2481cbf92cc5eddb7f34d3ae4837a
Ortega <ortega.romero.daniel@iescamas.es>
2026-02-23 13:32:59 +0100

    He extendido la figura geometrica

--- code/src/main/java/com/endes/figuras/FiguraGeometrica.java
+++ code/src/main/java/com/endes/figuras/FiguraGeometrica.java
@@ -1,7 +1,5 @@
 package com.endes.figuras;
 
-package com.endes.figuras;
-
 /**
  * Clase abstracta que representa una figura geométrica genérica.
  * Todas las figuras geométricas deben extender esta clase e implementar
@@ -9,7 +7,7 @@ package com.endes.figuras;
  * 
  * Implementa {@link Comparable} para proporcionar un orden natural basado en el área.
  * 
- * @author Tu nombre
+ * @author Ortega
  * @version 1.0
  * @since 1.8 
  */
---
+++ code/src/main/java/com/endes/figuras/Punto.java
@@ -0,0 +1,22 @@
+package com.endes.figuras;
+
+public class Punto extends FiguraGeometrica {
+
+	public Punto(String nombre, String color) {
+		super(nombre, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double perimetro() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
